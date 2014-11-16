bc2bdfdec50bad58458d51e2cbeb1fbc62d52928
ReimerGhost <rchamale10@gmail.com>
2014-11-15 19:59:03 -0600

    testing 1

--- src/com/usac/arqui2/chie/MainActivity.java
+++ src/com/usac/arqui2/chie/MainActivity.java
@@ -1,50 +1,56 @@
 package com.usac.arqui2.chie;
-
-import android.support.v7.app.ActionBarActivity;
-import android.content.Intent;
+ 
 import android.os.Bundle;
+import android.app.Activity;
+import android.content.Intent;
+import android.util.Log;
 import android.view.Menu;
-import android.view.MenuItem;
-
-
-public class MainActivity extends ActionBarActivity {
-
+import android.view.View;
+import android.widget.Button;
+ 
+public class MainActivity extends Activity {
+ 
+    private Button scan;
+     
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+         
+        scan= (Button)findViewById(R.id.bLeerQR);
+         
+        scan.setOnClickListener(new View.OnClickListener() {
+             
+            @Override
+            public void onClick(View v) {
+                // TODO Auto-generated method stub  
+                Intent intent = new Intent("com.google.zxing.client.android.SCAN");
+                intent.putExtra("SCAN_MODE", "QR_CODE_MODE");
+                startActivityForResult(intent, 0);
+            }
+        });
     }
-
-
+ 
+    public void onActivityResult(int requestCode, int resultCode, Intent intent) {
+           if (requestCode == 0) {
+              if (resultCode == RESULT_OK) {                  
+                 String contents = intent.getStringExtra("SCAN_RESULT");
+                 String format = intent.getStringExtra("SCAN_RESULT_FORMAT");
+              Log.i("CONTENTS", contents);
+              Log.i("FORMAT", format);
+                 // Handle successful scan
+                                           
+              } else if (resultCode == RESULT_CANCELED) {
+                 // Handle cancel
+                 Log.i("App","Scan unsuccessful");
+              }
+         }
+      }
+     
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.main, menu);
         return true;
     }
-    
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        if (requestCode == 0) {
-            if (resultCode == RESULT_OK) {
-                    String contents = data.getStringExtra("SCAN_RESULT"); //this is the result
-            } else 
-            if (resultCode == RESULT_CANCELED) {
-              // Handle cancel
-            }
-        }
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }        
 }
