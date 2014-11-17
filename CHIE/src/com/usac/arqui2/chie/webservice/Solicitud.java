a6229b23001f62bff2d782414e27483db0a85356
ReimerGhost <rchamale10@gmail.com>
2014-11-17 12:58:52 -0600

    KSOAP para Request funcionando. Falta para Insertar.

--- CHIE/src/com/usac/arqui2/chie/webservice/Solicitud.java
+++ CHIE/src/com/usac/arqui2/chie/webservice/Solicitud.java
@@ -3,6 +3,7 @@ package com.usac.arqui2.chie.webservice;
 import java.util.ArrayList;
 
 import org.ksoap2.SoapEnvelope;
+import org.ksoap2.serialization.PropertyInfo;
 import org.ksoap2.serialization.SoapObject;
 import org.ksoap2.serialization.SoapSerializationEnvelope;
 import org.ksoap2.transport.HttpTransportSE;
@@ -10,28 +11,35 @@ import org.ksoap2.transport.HttpTransportSE;
 import android.util.Log;
 
 public class Solicitud {
-	
-	public Solicitud(){
-		getDatos();	
+
+	public Solicitud() {
+		getDatos("10");
 	}
 
 	// Estos datos los proporciona el webService
-	final String NAMESPACE = "http://ws-chie.rhcloud.com/webservice/Servicios.php";
-	final String URL = "http://10.0.2.2/Visitas/WebServiceVisitas.asmx";
+	final String NAMESPACE = "http://ws-chie.rhcloud.com/webservice/Servicios.php?wsdl";
+	final String URL = "http://ws-chie.rhcloud.com/webservice/Servicios.php";
 
 	final String METHOD_NAME = "getMetrica";
 	final String SOAP_ACTION = "http://ws-chie.rhcloud.com/webservice/Servicios.php/getMetrica";
 
-	public void getDatos() {
+	public void getDatos(String dato) {
 		// Se instancia un objeto soap pasandole el namespace y el metodo del
 		// webservice a consumir
 		SoapObject request = new SoapObject(NAMESPACE, METHOD_NAME);
-
+		
+		
+		PropertyInfo GameId = new PropertyInfo();
+		GameId.setName("gameid");
+		GameId.setValue(dato);
+		request.addProperty(GameId);
+		
 		// Se instancia un objeto envelope y se define que version de soap se
 		// usará (10,11,12)
 		SoapSerializationEnvelope envelope = new SoapSerializationEnvelope(
 				SoapEnvelope.VER11);
 		envelope.setOutputSoapObject(request);
+		// envelope.dotNet = true;
 
 		// establece la comunicacion por http
 		// recordar habilitar permisos de internet
@@ -39,22 +47,21 @@ public class Solicitud {
 
 		try {
 			transporte.call(SOAP_ACTION, envelope); // ejecuta la llamada
-
-			SoapObject resSoap = (SoapObject) envelope.getResponse(); // obtiene
-																		// respuesta
+			// SoapObject resSoap = (SoapObject) envelope.getResponse(); //
+			// obtiene respuesta
+			SoapObject resSoap = (SoapObject) envelope.bodyIn;
 
 			// lo siguiente es para recorrer, leer y guardar la respuesta en una
 			// lista
 			int nPropiedades = resSoap.getPropertyCount();
 			for (int i = 0; i < nPropiedades; i++) {
-				SoapObject ic = (SoapObject) resSoap.getProperty(i);
-				String tipo = ic.getProperty("tipo").toString();
+				String tipo = resSoap.getProperty(i).toString();
 				System.out.println(tipo);
 				Log.i("WEBSER", tipo);
 			}
 		} catch (Exception e) {
 			e.printStackTrace();
-			Log.e("Error Webs", e.getMessage());
+			Log.e("Error Webs", e.toString());
 		}
 	}
 }
